e994b1064a6c197bb101ff142712293f63a24cb0
jannizz666 <janirisflopez@gmail.com>
2026-05-07 12:28:06 -0600

    inicializar conexion con firebase

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
+/*estado global*/
+
 let db = null;
 let filterCat = 'all';
+
+/*firebase inicializacion*/
+
+function initFirebase(){
+    try{
+        initFirebase.initializeApp(firebaseConfig);
+        db = firebase.database();
+
+        db.ref('.info/connected').on('value', snap => {
+            setDbStatus(snap.val()=== true ? 'connected' : 'disconnected');
+
+        });
+        startListening();
+    
+    } catch (e) {
+        showToast('Error al conectar con Firebase: ' + e.message, 'error');
+        setDbStatus('error');
+    }
+}
